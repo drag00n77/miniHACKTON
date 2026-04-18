e2492236f49ec738131884961baae3be39a64c3e
drag00n77 <neupaneankit072@gmail.com>
2026-04-18 11:17:37 -0400

    go backend update

--- backend.go
+++ backend.go
@@ -3,7 +3,30 @@ package main
 import(
   "fmt"
   "net/http"
+  "encoding/json"
+  "math/rand"
 )
+
+type Enemy struct {
+
+    ID int `json:"id"`
+    Floor int `json:"floor"`
+    X int `json:"x"`
+}
+type Player struct {
+    Floor int  `json:"floor"`
+    Alive bool `json:"alive"`
+    Score int  `json:"score"`
+}
+type GameState struct {
+    Player  Player  `json:"player"`
+    Enemies []Enemy `json:"enemies"`
+}
+var state = GameState{
+    Player: Player{Floor: 1, Alive: true, Score: 0},
+}
+
+var enemyID = 0
 
 func game(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Game is running!")
