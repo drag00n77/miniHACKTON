099b5547fce54219a41671cc57d30f5a0094e152
drag00n77 <neupaneankit072@gmail.com>
2026-04-18 10:41:10 -0400

    go backend has been started

--- backend.go
+++ backend.go
@@ -1,8 +1,16 @@
-package main
+package main 
 
-import "fmt"
+import(
+  "fmt"
+  "net/http"
+)
 
-func main(){
-fmt.Println("hello world, set up project")
+func game(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Game is running!")
+}
 
+func main() {
+    http.HandleFunc("/", game)
+    fmt.Println("Server on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
